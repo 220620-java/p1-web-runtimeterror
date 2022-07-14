e9bb3ebe14a79beeae2cb65cd09bf21adad9c6b1
Hannah Bush <heb.rutgers@gmail.com>
2022-07-14 14:06:41 -0400

    player model created

--- src/main/java/com/revature/p1/orm/models/Player.java
+++ src/main/java/com/revature/p1/orm/models/Player.java
@@ -1,6 +1,63 @@
 package com.revature.p1.orm.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
-	//blah
-	new Character();
+    private int id;
+    private String name, username;
+    private List<Avatars> avatars;
+
+
+    //------------------------ classes -----------------------
+    public Player (){
+        super();
+        this.id = 0;
+        this.name = "";
+        this.username = "";
+        this.avatars = new ArrayList();
+    }
+
+    public Player (String username) {
+        super();
+        this.username = username;
+    }
+
+    public Player (int id, String name, String username){
+        super();
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.avatars = new ArrayList();
+    }
+
+    //------------------------ methods -----------------------
+    @Override
+    public String toString() {
+        return "Player [id=" + id + ", name =" + name + ", username=" + username + "avatars=" + avatars + "]";
+    }
+
+    //------------------------ getters & setters -----------------------
+    //get & set id------------------------------------------------------------------
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    //get & set name------------------------------------------------------------------
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    //get & set username------------------------------------------------------------------
+    public String getUsername(){
+        return username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
 }
