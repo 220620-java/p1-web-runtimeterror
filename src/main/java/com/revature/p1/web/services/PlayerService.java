45a800a2f044b534e4c8aea5fba4c7cbca310933
Fuller <fullerj@wwt.com>
2022-07-19 15:44:04 -0500

    playerservice commit

--- src/main/java/com/revature/p1/web/services/PlayerService.java
+++ src/main/java/com/revature/p1/web/services/PlayerService.java
@@ -1,5 +1,20 @@
 package com.revature.p1.web.services;
 
+import java.util.List;
+
+import com.revature.p1.web.models.Avatar;
+import com.revature.p1.web.models.Player;
+
 public interface PlayerService {
+	
+	public Player registerUsername(Player player)throws UsernameAlreadyExistsException;
+	
+	
+	public List<Avatar> viewAllAvatars();
+	
+	
+	
+	public Player getPlayer(int id);
+	
 
 }
