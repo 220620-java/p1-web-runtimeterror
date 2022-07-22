137f2f5b74bc96df7f0d84a05f2915f655eed1b6
Fuller <fullerj@wwt.com>
2022-07-21 22:52:34 -0500

    to add username exception

--- src/main/java/com/revature/p1/web/exceptions/UsernameAlreadyExistsException.java
+++ src/main/java/com/revature/p1/web/exceptions/UsernameAlreadyExistsException.java
@@ -4,5 +4,12 @@ import java.sql.SQLException;
 
 public class UsernameAlreadyExistsException extends Exception {
 	
+	private Error code;
 
+	public UsernameAlreadyExistsException(String message, Throwable cause, Error code) {
+		
+		super(message, cause);
+		this.code = code;
+	}
+	//for commit
 }
