9d66c00e8720ebd12d136dcf18563095e9ba2060
Hannah Bush <heb.rutgers@gmail.com>
2022-07-22 00:44:49 -0400

    fixed player orm exception error

--- src/main/java/com/revature/p1/web/data/DataAccessObject.java
+++ src/main/java/com/revature/p1/web/data/DataAccessObject.java
@@ -1,8 +1,9 @@
 package com.revature.p1.web.data;
 
+import java.sql.SQLException;
 import java.util.List;
 public interface DataAccessObject<T> {
-	public T create(T t);
+	public T create(T t) throws SQLException;
 	
 	public T findById(int id);
 	
